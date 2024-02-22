22a0ffeb8a6b7b2b1df5f33299d3eb20dadd074e
Souvik Kundu <160837710+KunduSouvik24@users.noreply.github.com>
2024-02-22 17:50:07 +0000

    Done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail = "Souvik@google.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+
+// accountId = 2 // *not allowed
+
+// accountEmail = "hc@hc.com"
+// accountPassword = "2121221221"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity])
