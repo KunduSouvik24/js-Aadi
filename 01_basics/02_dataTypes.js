6729dbd62b72c8d368d04bbd8898316b10ff96d8
Souvik Kundu <160837710+KunduSouvik24@users.noreply.github.com>
2024-02-23 03:39:12 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3
+    +
+     3)
+
+console.log("Souvik");
+
+let name = "Souvik"
+let age = 23
+let isLoggedIn = false
+
+/* number => 2 to power 53
+   bigint
+   string => ""
+   boolean => true/false
+   null => standalone value
+   undefined => 
+   symbol => unique
+
+   object
+*/
+
+console.log(typeof age);
+console.log(typeof "Souvik");
+console.log(typeof null);
+console.log(typeof undefined);
